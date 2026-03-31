66c7a6030de9f536161cb05bcfb3adbaa0f4f7ca
arugoa <arihantjaingoa@gmail.com>
2026-03-30 19:36:20 -0700

    yolox working with sim

--- yolox_ros_cpp/yolox_ros_cpp/include/yolox_ros_cpp/yolox_ros_cpp.hpp
+++ yolox_ros_cpp/yolox_ros_cpp/include/yolox_ros_cpp/yolox_ros_cpp.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <cmath>
 #include <chrono>
+#include <cmath>
 
 #if __has_include(<cv_bridge/cv_bridge.hpp>)
 #include <cv_bridge/cv_bridge.hpp>
@@ -18,43 +18,48 @@
 #include "bboxes_ex_msgs/msg/bounding_box.hpp"
 #include "bboxes_ex_msgs/msg/bounding_boxes.hpp"
 
-#include "yolox_cpp/yolox.hpp"
 #include "yolox_cpp/utils.hpp"
+#include "yolox_cpp/yolox.hpp"
 #include "yolox_param/yolox_param.hpp"
 
 #include "tr_messages/msg/det_with_img.hpp"
 
-#include "shm/SharedDetWithImg.h"
+#include "shm/SharedDetWithImage.h"
 
-namespace yolox_ros_cpp{
-    class YoloXNode : public rclcpp::Node
-    {
-    public:
-        YoloXNode(const rclcpp::NodeOptions &);
-    private:
-        void onInit();
-        void colorImageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &);
+namespace yolox_ros_cpp {
+class YoloXNode : public rclcpp::Node {
+  public:
+    YoloXNode(const rclcpp::NodeOptions &);
 
-        static bboxes_ex_msgs::msg::BoundingBoxes objects_to_bboxes(const cv::Mat &, const std::vector<yolox_cpp::Object> &, const std_msgs::msg::Header &);
-        static vision_msgs::msg::Detection2DArray objects_to_detection2d(const std::vector<yolox_cpp::Object> &, const std_msgs::msg::Header &);
-        static Detection2DArray objects_to_shm_detection2darray(const std::vector<yolox_cpp::Object> &,
-                                const long &);
-    protected:
-        std::shared_ptr<yolox_parameters::ParamListener> param_listener_;
-        yolox_parameters::Params params_;
-    private:
-        std::unique_ptr<yolox_cpp::AbcYoloX> yolox_;
-        std::vector<std::string> class_names_;
+  private:
+    void onInit();
+    void colorImageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &);
 
-        rclcpp::TimerBase::SharedPtr init_timer_;
-        image_transport::Subscriber sub_image_;
+    static bboxes_ex_msgs::msg::BoundingBoxes
+    objects_to_bboxes(const cv::Mat &, const std::vector<yolox_cpp::Object> &,
+                      const std_msgs::msg::Header &);
+    static vision_msgs::msg::Detection2DArray
+    objects_to_detection2d(const std::vector<yolox_cpp::Object> &, const std_msgs::msg::Header &);
+    static Detection2DArray objects_to_shm_detection2darray(const std::vector<yolox_cpp::Object> &,
+                                                            const long &);
 
-        struct timespec curr_ts_;
+  protected:
+    std::shared_ptr<yolox_parameters::ParamListener> param_listener_;
+    yolox_parameters::Params params_;
 
-        std::unique_ptr<SharedDetWithImageWriter> sharedDetWriter_;
+  private:
+    std::unique_ptr<yolox_cpp::AbcYoloX> yolox_;
+    std::vector<std::string> class_names_;
 
-        rclcpp::Publisher<bboxes_ex_msgs::msg::BoundingBoxes>::SharedPtr pub_bboxes_;
-        rclcpp::Publisher<tr_messages::msg::DetWithImg>::SharedPtr pub_detection2d_;
-        image_transport::Publisher pub_image_;
-    };
-}
+    rclcpp::TimerBase::SharedPtr init_timer_;
+    image_transport::Subscriber sub_image_;
+
+    struct timespec curr_ts_;
+
+    std::unique_ptr<SharedDetWithImageWriter> sharedDetWriter_;
+
+    rclcpp::Publisher<bboxes_ex_msgs::msg::BoundingBoxes>::SharedPtr pub_bboxes_;
+    rclcpp::Publisher<tr_messages::msg::DetWithImg>::SharedPtr pub_detection2d_;
+    image_transport::Publisher pub_image_;
+};
+} // namespace yolox_ros_cpp
